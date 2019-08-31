c4f9680f5358b4906e1fa00a455e9f6f092cc05f
Austin Robbins <austinrobbins@Austins-MBP.hsd1.wa.comcast.net>
2019-08-31 14:13:12 -0700

    psuedo code the rest

---
+++ controllers/burger_controller.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const burger = require('../models/burger');
+
+//create the route for the App and export the router at the end of the file 
---
+++ models/burger.js
@@ -0,0 +1,5 @@
+const orm = require('../config/orm');
+
+// create the dode that will call the orm functions using burger specific input for the ORM 
+
+module.exports = something
